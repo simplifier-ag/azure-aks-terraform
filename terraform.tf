6eeaf2aa038668910d2f852dff2b76d4ad9186a4
gretel <code@jitter.eu>
2022-02-07 16:15:19 +0100

    bump provider

--- terraform.tf
+++ terraform.tf
@@ -17,7 +17,7 @@ terraform {
     # https://registry.terraform.io/providers/hashicorp/azurerm/latest
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.94.0"
+      version = "~> 2.95.0"
     }
     # https://registry.terraform.io/providers/microsoft/azuredevops/latest
     azuredevops = {
